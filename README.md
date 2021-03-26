2cc8ee3a7d9a245f74e0c4af12d254bd820cd17f
Regan <regdshaner@gmail.com>
2021-03-26 11:14:22 -0400

    Added database configuration

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Strapi application
 
 A quick description of your strapi application
+
+## Local Development
+Begin by creating a local postgres database for your strapi application to write to.
+
+`docker run --name strapi-postgres -e POSTGRES_DB=strapidb -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres`
+
+
--- config/database.js
+++ config/database.js
@@ -5,7 +5,13 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapidb'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
+        schema: 'public',
       },
       options: {
         useNullAsDefault: true,
